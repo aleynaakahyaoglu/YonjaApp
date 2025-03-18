dc2429cc0d83b016c8b745c1c134223ba036d305
Aleyna <akahyaoglu00@gmail.com>
2025-03-18 21:46:47 +0300

    Initial Commit

---
+++ Yonja/YonjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YonjaApp.swift
+//  Yonja
+//
+//  Created by Aleyna on 18.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct YonjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
